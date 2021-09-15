c08b535484e03e404fb8269dd6033861be60efad
Daniel Mathews <sir.daniel.mathews@gmail.com>
2021-09-15 13:22:09 -0700

    Initial Commit

---
+++ ChessMobile/ChessMobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChessMobileApp.swift
+//  ChessMobile
+//
+//  Created by Daniel Mathews on 9/15/21.
+//
+
+import SwiftUI
+
+@main
+struct ChessMobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChessMobile/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ChessMobile
+//
+//  Created by Daniel Mathews on 9/15/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
